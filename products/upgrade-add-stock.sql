6884e2b4bc9d848b9f62c4f492cda6160be1ea24
pferguson1 <4720433+pferguson1@users.noreply.github.com>
2025-12-09 20:36:31 -0500

    Delete products directory

--- products/upgrade-add-stock.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `products` ADD COLUMN `stock` int(11) NOT NULL DEFAULT 100;
-UPDATE `products` SET stock=50 WHERE sku='IPHO001';
-UPDATE `products` SET stock=35 WHERE sku='CAME001';
-UPDATE `products` SET stock=120 WHERE sku='WATC001';
